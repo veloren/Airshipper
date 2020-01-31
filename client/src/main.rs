0c714dcc41373aa58865bf0b5be73b17cbf14054
Songtronix <contact@songtronix.com>
2020-01-31 16:40:37 +0100

    fix: reduce log file size and improved logging on windows

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,3 @@
-// https://github.com/rust-lang/rust/pull/40870
-#![windows_subsystem = "windows"]
-
 mod cli;
 mod error;
 mod filesystem;
@@ -16,13 +13,6 @@ pub type Result<T> = std::result::Result<T, ClientError>;
 
 #[async_std::main]
 async fn main() {
-    // Required on windows due to stdin/stdout not being attached by default
-    // to the parent process if `#![windows_subsystem = "windows"]` is used.
-    #[cfg(windows)]
-    unsafe {
-        let _ = winapi::um::wincon::AttachConsole(winapi::um::wincon::ATTACH_PARENT_PROCESS);
-    }
-    
     if let Err(e) = cli::process().await {
         log::error!("{}", e);
         log::info!("Press enter to exit...");
