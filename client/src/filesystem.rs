c1c41605ab0c9d66cdc30b2064814b758084730f
Songtronix <contact@songtronix.com>
2020-02-01 18:47:33 +0100

    fix: warning

--- client/src/filesystem.rs
+++ client/src/filesystem.rs
@@ -12,8 +12,8 @@ pub const VOXYGEN_FILE: &str = "veloren-voxygen.exe";
 #[cfg(unix)]
 pub const VOXYGEN_FILE: &str = "veloren-voxygen";
 
-#[cfg(windows)]
-pub const SERVER_CLI_FILE: &str = "veloren-server-cli.exe";
+//#[cfg(windows)]
+//pub const SERVER_CLI_FILE: &str = "veloren-server-cli.exe";
 #[cfg(unix)]
 pub const SERVER_CLI_FILE: &str = "veloren-server-cli";
 
