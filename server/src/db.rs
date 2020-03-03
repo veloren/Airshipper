7751272b60173eaa89dd1ebe0e3051701bbaffdc
Songtronix <contact@songtronix.com>
2020-03-03 17:18:28 +0100

    simplify env key loading
    customized formatting
    load `.airshipper-env` for env vars

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,7 +1,10 @@
-use crate::models::{Channel, Platform};
-use crate::Result;
+use crate::{
+    models::{Channel, Platform},
+    Result,
+};
 
 // TODO: Needs to track releases too!
+// TODO: Simply store it in a ron file lol.
 
 pub fn init() -> Result<Database> {
     Ok(Database {
@@ -21,20 +24,12 @@ pub struct Database {
 /// Latest is hardcoded for now till support for multiple nightlies is there.
 
 impl Database {
-    pub fn get_latest_channel_version(
-        &self,
-        platform: Platform,
-        channel: Channel,
-    ) -> Result<Option<String>> {
+    pub fn get_latest_channel_version(&self, platform: Platform, channel: Channel) -> Result<Option<String>> {
         use std::str;
         Ok(self
             .db
             .get(&format!("version.latest.{}.{}", platform, channel))?
-            .map(|x| {
-                str::from_utf8(&x)
-                    .expect("FATAL: Corrupted database!")
-                    .to_string()
-            }))
+            .map(|x| str::from_utf8(&x).expect("FATAL: Corrupted database!").to_string()))
     }
 
     pub fn get_latest_uri(&self, platform: Platform, channel: Channel) -> Result<Option<String>> {
@@ -42,26 +37,12 @@ impl Database {
         Ok(self
             .db
             .get(&format!("uri.latest.{}.{}", platform, channel))?
-            .map(|x| {
-                str::from_utf8(&x)
-                    .expect("FATAL: Corrupted database!")
-                    .to_string()
-            }))
+            .map(|x| str::from_utf8(&x).expect("FATAL: Corrupted database!").to_string()))
     }
 
-    pub fn update_artifact(
-        &mut self,
-        platform: Platform,
-        channel: Channel,
-        hash: &str,
-        download_uri: &str,
-    ) -> Result<()> {
-        self.db
-            .insert(&format!("version.latest.{}.{}", platform, channel), hash)?;
-        self.db.insert(
-            &format!("uri.latest.{}.{}", platform, channel),
-            download_uri,
-        )?;
+    pub fn update_artifact(&mut self, platform: Platform, channel: Channel, hash: &str, download_uri: &str) -> Result<()> {
+        self.db.insert(&format!("version.latest.{}.{}", platform, channel), hash)?;
+        self.db.insert(&format!("uri.latest.{}.{}", platform, channel), download_uri)?;
         Ok(())
     }
 }
