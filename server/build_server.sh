0f57c657992de841a54ae84e4f7a2603ae0a42de
Songtronix <contact@songtronix.com>
2020-07-07 11:32:08 +0200

    add(server): scripts to build image

---
+++ server/build_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# NOTE: EXECUTE THIS FROM THE WORKSPACE ROOT ONLY
+cp Cargo.lock server/Cargo.lock
+sleep 1
+sudo docker build server/ -f server/Dockerfile -t docker.pkg.github.com/songtronix/airshipper/airshipper:latest
+sleep 1
+rm server/Cargo.lock
+sleep 1
