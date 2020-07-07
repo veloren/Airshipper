87c372323cfb9b54713ebad53e481d96d85f2c97
Songtronix <contact@songtronix.com>
2020-07-07 12:19:39 +0200

    fix: validate hash before uploading and updating db

--- server/src/webhook.rs
+++ server/src/webhook.rs
@@ -2,7 +2,7 @@ use crate::{error::ServerError, models::Artifact, Result};
 use reqwest::StatusCode;
 
 pub fn process(artifacts: Vec<Artifact>, mut db: crate::DbConnection) {
-    tokio::task::spawn(async move {
+    tokio::spawn(async move {
         for artifact in artifacts {
             if let Err(e) = transfer(artifact, &mut db).await {
                 tracing::error!("Failed to transfer artifact: {}.", e);
@@ -22,29 +22,60 @@ async fn transfer(artifact: Artifact, db: &mut crate::DbConnection) -> Result<()
 
     let mut resp = reqwest::get(&artifact.get_url()).await?;
     let mut file = File::create(&artifact.file_name).await?;
+    let mut content = vec![];
     while let Some(chunk) = resp.chunk().await? {
         file.write_all(&chunk).await?;
+        content.write_all(&chunk).await?;
     }
+    file.sync_data().await?;
 
-    tracing::info!("Uploading...");
-    let code = crate::S3Connection::new().await?.upload(&artifact).await?;
+    let hash = format!("{:x}", md5::compute(content));
+    let remote_hash = get_remote_hash(&resp);
 
-    // Delete obselete artifact
-    let _ = std::fs::remove_file(&artifact.file_name);
-
-    if is_success(code) {
-        // Update database with new information
-        tracing::info!("Update database...");
-        db.insert_artifact(artifact)?;
-        Ok(())
+    if hash != remote_hash {
+        tracing::error!(
+            "Downloaded file has '{}' MD5 hash while remote hash is '{}'. Exiting...",
+            hash,
+            remote_hash
+        );
+        // Clean up
+        tokio::fs::remove_file(&artifact.file_name).await?;
     } else {
-        Err(ServerError::InvalidResponseCode(
-            StatusCode::from_u16(code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
-            artifact,
-        ))
+        tracing::debug!("Computed hash: {}, remote_hash: {}", hash, remote_hash);
+        tracing::info!("Uploading...");
+        let code = crate::S3Connection::new().await?.upload(&artifact).await?;
+
+        if is_success(code) {
+            tracing::debug!("Validating remote hash...");
+            let uploaded_hash = get_remote_hash(&reqwest::get(&artifact.download_uri).await?);
+            if uploaded_hash != hash {
+                tracing::error!("Uploaded file is corrupted! Deleting...");
+                crate::S3Connection::new().await?.delete(&artifact).await?;
+            } else {
+                // Update database with new information
+                tracing::info!("Remote hash valid. Update database...");
+                db.insert_artifact(&artifact)?;
+            }
+        } else {
+            return Err(ServerError::InvalidResponseCode(
+                StatusCode::from_u16(code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
+                artifact,
+            ));
+        }
+        // Delete obselete artifact
+        tokio::fs::remove_file(&artifact.file_name).await?;
     }
+    Ok(())
 }
 
 fn is_success(code: u16) -> bool {
     code < 399 && code > 199
+}
+
+fn get_remote_hash(resp: &reqwest::Response) -> String {
+    resp.headers()
+        .get(reqwest::header::ETAG)
+        .map(|x| x.to_str().expect("always valid ascii?"))
+        .unwrap_or("REMOTE_ETAG_MISSING")
+        .replace("\"", "")
 }
