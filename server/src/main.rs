ea8ff03fd916e8852e1388740b5718776ae0c4eb
Songtronix <contact@songtronix.com>
2020-07-19 11:27:07 +0200

    fix: update rocket to master branch

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,8 +4,9 @@
 // curl --header "Content-Type: application/json" --request POST --data "@<FILE_WITH_WEBHOOK_DATA>" --header "X-Gitlab-Event: Pipeline Hook" --header "X-Gitlab-Token: <TOKEN>" http://<ADDRESS>
 mod logger;
 
-fn main() {
+#[rocket::launch]
+fn rocket() -> rocket::Rocket {
     dotenv::from_path("server/.env").ok();
     logger::init();
-    server::rocket().launch().expect("Server failed!");
+    server::rocket()
 }
