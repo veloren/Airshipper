cf49340e3aabed7ae928bc0d5b05cc3a840bf49a
Songtronix <contact@songtronix.com>
2020-08-05 12:09:08 +0200

    fix(server): adjust Dockerfile & script

--- server/publish_server.sh
+++ server/publish_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# NOTE: EXECUTE THIS FROM THE WORKSPACE ROOT ONLY
-sudo docker build server/ -f server/Dockerfile -t docker.pkg.github.com/songtronix/airshipper/airshipper:latest
+# NOTE: Run `build_server.sh` beforehand!
 sudo docker push docker.pkg.github.com/songtronix/airshipper/airshipper:latest
