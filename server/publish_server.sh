873c3521f5e0a4947a4d68cb499199fb905de211
Songtronix <contact@songtronix.com>
2021-03-26 21:28:56 +0100

    fix(gitlab): update pipeline model

--- server/publish_server.sh
+++ server/publish_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # NOTE: Run `build_server.sh` beforehand!
-sudo docker push docker.pkg.github.com/songtronix/airshipper/airshipper:latest
+sudo docker push docker.pkg.github.com/songtronix/airshipper/airshipper:master
