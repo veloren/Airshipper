8e47e3dc31dcc96874b23b480c5130e713f5e7ba
Songtronix <contact@songtronix.com>
2021-04-05 17:02:47 +0200

    fix(client): reduce minimal window size

--- client/src/gui/mod.rs
+++ client/src/gui/mod.rs
@@ -260,7 +260,7 @@ fn settings(cmd: CmdLine) -> Settings<CmdLine> {
                 Icon::from_rgba(icon.to_rgba8().into_raw(), icon.width(), icon.height())
                     .unwrap(),
             ),
-            min_size: Some((1050, 620)),
+            min_size: Some((400, 250)),
             ..Default::default()
         },
         flags: cmd,
