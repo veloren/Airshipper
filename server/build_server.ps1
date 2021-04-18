764bf186ea6af0d0b87aa7f2dd46540d75b97263
Songtronix <contact@songtronix.com>
2021-04-18 14:55:28 +0200

    change(server): use new ghcr.io registry

--- server/build_server.ps1
+++ server/build_server.ps1
@@ -4,7 +4,7 @@ if (!($tag = Read-Host "Docker Image Tag [$default]")) { $tag = $default }
 
 Copy-Item Cargo.lock server/Cargo.lock
 sleep 1
-docker build server/ -f server/Dockerfile -t docker.pkg.github.com/songtronix/airshipper/airshipper:$tag
+docker build server/ -f server/Dockerfile -t ghcr.io/songtronix/airshipper:$tag
 sleep 1
 Remove-Item server/Cargo.lock -ErrorAction Ignore
 sleep 1
--- server/publish_server.sh
+++ server/publish_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # NOTE: Run `build_server.sh` beforehand!
-sudo docker push docker.pkg.github.com/songtronix/airshipper/airshipper:master
+sudo docker push ghcr.io/songtronix/airshipper:master
