ca8bf793684cae5c87197a382e5d4151d313340c
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-05-12 19:17:48 +0300

    feat: implement nixos specific executable patching

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flakeCompat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     nixCargoIntegration = {
       url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
