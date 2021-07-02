9074a00a36b46614146cbc7e3289efc0bcea5d59
Marcel Märtens <marcel.cochem@googlemail.com>
2021-07-02 18:31:21 +0200

    various scripts removed, fix another clippy

--- server/publish_server.ps1
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# NOTE: Run `build_server.ps1` beforehand!
-$default = "master"
-if (!($tag = Read-Host "Docker Image Tag [$default]")) { $tag = $default }
-
-docker push docker.pkg.github.com/veloren/airshipper/airshipper:$tag
