5c05372c6643016e4be3f47530b3d78aeb4dcb90
Marcel Märtens <marcel.cochem@googlemail.com>
2021-07-08 13:04:42 +0200

    get rid of lib.rs file

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,11 +2,72 @@
 
 // How to send manual webhooks:
 // curl --header "Content-Type: application/json" --request POST --data "@<FILE_WITH_WEBHOOK_DATA>" --header "X-Gitlab-Event: Pipeline Hook" --header "X-Gitlab-Token: <TOKEN>" http://<ADDRESS>
+
+use rocket::{fs::FileServer, *};
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+
+mod config;
+mod db;
+mod error;
+mod fairings;
+mod guards;
+mod metrics;
+mod models;
+mod prune;
+mod routes;
+mod webhook;
 mod logger;
+
+use crate::error::ServerError;
+use config::{ServerConfig, LOCAL_STORAGE_PATH};
+use metrics::Metrics;
+
+pub type Result<T> = std::result::Result<T, ServerError>;
+pub use db::{DbConnection, FsStorage};
+
+lazy_static::lazy_static! {
+    /// Contains all configuration needed.
+    pub static ref CONFIG: ServerConfig = ServerConfig::load();
+}
 
 #[rocket::launch]
 async fn rocket() -> _ {
     dotenv::from_path("server/.env").ok();
     logger::init();
-    airshipper_server::rocket().await.unwrap()
+    build().await.unwrap()
+}
+
+async fn build() -> Result<rocket::Rocket<rocket::Build>> {
+    let local_storage_folder = CONFIG.get_local_storage_path();
+    if !local_storage_folder.exists() {
+        tokio::fs::create_dir_all(local_storage_folder.clone())
+            .await
+            .unwrap();
+    }
+
+    // Base of the config and attach everything else
+    Ok(CONFIG
+        .rocket()
+        .attach(DbConnection::fairing())
+        .attach(fairings::db::DbInit)
+        .manage(Metrics::new().expect("Failed to create prometheus statistics!"))
+        .mount("/", routes![
+            routes::gitlab::post_pipeline_update,
+            routes::user::index,
+            routes::user::robots,
+            routes::user::favicon,
+            routes::api::version,
+            routes::api::channel_version,
+            routes::api::download,
+            routes::api::channel_download,
+            routes::metrics::metrics,
+        ])
+        .mount(
+            &format!("/{}", LOCAL_STORAGE_PATH),
+            FileServer::from(local_storage_folder),
+        )
+        .register("/", catchers![routes::catchers::not_found]))
 }
