e5dc22cbd39308618716f1e73580c3c06d96924f
Ben Wallis <atomyc@gmail.com>
2021-08-28 17:14:37 +0100

    Refactored style.rs to use constants for colors.

--- client/src/gui/style.rs
+++ client/src/gui/style.rs
@@ -6,23 +6,38 @@ pub enum PrimaryButton {
     Enabled,
     Disabled,
 }
+
+// Colors
+const LIGHT_BLUE: Color = Color::from_rgb(0.05, 0.44, 0.62);
+const BROWN: Color = Color::from_rgb(0.29, 0.19, 0.03);
+const LIGHT_GREY: Color = Color::from_rgb(0.93, 0.93, 0.93);
+const MEDIUM_GREY: Color = Color::from_rgb(0.7, 0.7, 0.7);
+const DARK_TEAL: Color = Color::from_rgb(0.10, 0.21, 0.25);
+const MEDIUM_TEAL: Color = Color::from_rgb(0.09, 0.24, 0.29);
+const LIGHT_TEAL: Color = Color::from_rgb(0.14, 0.29, 0.35);
+const LIGHT_SEA_GREEN: Color = Color::from_rgb(0.35, 0.82, 0.76);
+const SEA_GREEN: Color = Color::from_rgb(0.18, 0.65, 0.59);
+const DARKER_SEA_GREEN: Color = Color::from_rgb(0.08, 0.61, 0.65);
+const SLATE: Color = Color::from_rgb(0.35, 0.43, 0.46);
+const TRANSPARENT_WHITE: Color = Color::from_rgba(1.0, 1.0, 1.0, 0.1);
+
 impl button::StyleSheet for PrimaryButton {
     fn active(&self) -> button::Style {
         match self {
             Self::Enabled => button::Style {
-                background: Some(Background::Color(Color::from_rgb(0.05, 0.44, 0.62))),
-                border_color: Color::from_rgb(0.29, 0.19, 0.03),
+                background: Some(Background::Color(LIGHT_BLUE)),
+                border_color: BROWN,
                 border_width: 4.0,
                 shadow_offset: Vector::new(1.0, 1.0),
-                text_color: Color::from_rgb8(0xEE, 0xEE, 0xEE),
+                text_color: LIGHT_GREY,
                 ..button::Style::default()
             },
             Self::Disabled => button::Style {
-                background: Some(Background::Color(Color::from_rgb(0.35, 0.43, 0.46))),
-                border_color: Color::from_rgb(0.29, 0.19, 0.03),
+                background: Some(Background::Color(SLATE)),
+                border_color: BROWN,
                 border_width: 4.0,
                 shadow_offset: Vector::new(1.0, 1.0),
-                text_color: Color::from_rgb8(0xEE, 0xEE, 0xEE),
+                text_color: LIGHT_GREY,
                 ..button::Style::default()
             },
         }
@@ -31,14 +46,14 @@ impl button::StyleSheet for PrimaryButton {
     fn hovered(&self) -> button::Style {
         match self {
             Self::Enabled => button::Style {
-                background: Some(Background::Color(Color::from_rgb(0.08, 0.61, 0.65))),
+                background: Some(Background::Color(DARKER_SEA_GREEN)),
                 text_color: Color::WHITE,
                 shadow_offset: Vector::new(1.0, 2.0),
                 ..self.active()
             },
             Self::Disabled => button::Style {
-                background: Some(Background::Color(Color::from_rgb8(91, 110, 117))),
-                text_color: Color::from_rgb8(0xEE, 0xEE, 0xEE),
+                background: Some(Background::Color(SLATE)),
+                text_color: LIGHT_GREY,
                 shadow_offset: Vector::new(1.0, 2.0),
                 ..self.active()
             },
@@ -50,15 +65,15 @@ pub struct SecondaryButton;
 impl button::StyleSheet for SecondaryButton {
     fn active(&self) -> button::Style {
         button::Style {
-            background: Some(Background::Color(Color::from_rgb(0.05, 0.44, 0.62))),
-            text_color: Color::from_rgb8(0xEE, 0xEE, 0xEE),
+            background: Some(Background::Color(LIGHT_BLUE)),
+            text_color: LIGHT_GREY,
             ..button::Style::default()
         }
     }
 
     fn hovered(&self) -> button::Style {
         button::Style {
-            background: Some(Background::Color(Color::from_rgb(0.08, 0.61, 0.65))),
+            background: Some(Background::Color(DARKER_SEA_GREEN)),
             text_color: Color::WHITE,
             ..self.active()
         }
@@ -77,7 +92,7 @@ impl button::StyleSheet for SettingsButton {
 
     fn hovered(&self) -> button::Style {
         button::Style {
-            background: Some(Background::Color(Color::from_rgba(1.0, 1.0, 1.0, 0.1))),
+            background: Some(Background::Color(TRANSPARENT_WHITE)),
             ..self.active()
         }
     }
@@ -88,8 +103,8 @@ impl pick_list::StyleSheet for ServerPickList {
     fn menu(&self) -> pick_list::Menu {
         pick_list::Menu {
             text_color: Color::WHITE,
-            background: Background::Color(Color::from_rgb(0.10, 0.21, 0.25)),
-            selected_background: Background::Color(Color::from_rgb(0.18, 0.65, 0.59)),
+            background: Background::Color(DARK_TEAL),
+            selected_background: Background::Color(SEA_GREEN),
             selected_text_color: Color::WHITE,
             ..pick_list::Menu::default()
         }
@@ -98,7 +113,7 @@ impl pick_list::StyleSheet for ServerPickList {
     fn active(&self) -> pick_list::Style {
         pick_list::Style {
             text_color: Color::WHITE,
-            background: Background::Color(Color::from_rgb(0.10, 0.21, 0.25)),
+            background: Background::Color(DARK_TEAL),
             icon_size: 0.5,
             ..pick_list::Style::default()
         }
@@ -115,7 +130,7 @@ pub struct News;
 impl container::StyleSheet for News {
     fn style(&self) -> container::Style {
         container::Style {
-            background: Some(Background::Color(Color::from_rgb(0.09, 0.24, 0.29))),
+            background: Some(Background::Color(MEDIUM_TEAL)),
             text_color: Some(Color::WHITE),
             ..container::Style::default()
         }
@@ -126,10 +141,10 @@ pub struct Middle;
 impl container::StyleSheet for Middle {
     fn style(&self) -> container::Style {
         container::Style {
-            background: Some(Background::Color(Color::from_rgb(0.10, 0.21, 0.25))),
+            background: Some(Background::Color(DARK_TEAL)),
             text_color: Some(Color::WHITE),
             border_width: 2.0,
-            border_color: Color::from_rgb(0.14, 0.29, 0.35),
+            border_color: LIGHT_TEAL,
             ..container::Style::default()
         }
     }
@@ -139,10 +154,10 @@ pub struct Bottom;
 impl container::StyleSheet for Bottom {
     fn style(&self) -> container::Style {
         container::Style {
-            background: Some(Background::Color(Color::from_rgb(0.10, 0.21, 0.25))),
+            background: Some(Background::Color(DARK_TEAL)),
             text_color: Some(Color::WHITE),
             border_width: 2.0,
-            border_color: Color::from_rgb(0.14, 0.29, 0.35),
+            border_color: LIGHT_TEAL,
             ..container::Style::default()
         }
     }
@@ -152,8 +167,8 @@ pub struct Progress;
 impl progress_bar::StyleSheet for Progress {
     fn style(&self) -> progress_bar::Style {
         progress_bar::Style {
-            background: Background::Color(Color::from_rgb(0.35, 0.43, 0.46)),
-            bar: Background::Color(Color::from_rgb(0.35, 0.82, 0.76)),
+            background: Background::Color(SLATE),
+            bar: Background::Color(LIGHT_SEA_GREEN),
             border_radius: 5.0,
         }
     }
@@ -163,7 +178,7 @@ pub struct Content;
 impl container::StyleSheet for Content {
     fn style(&self) -> container::Style {
         container::Style {
-            background: Some(Background::Color(Color::from_rgb(0.10, 0.21, 0.25))),
+            background: Some(Background::Color(DARK_TEAL)),
             text_color: Some(Color::WHITE),
             ..container::Style::default()
         }
@@ -175,9 +190,9 @@ pub struct Tooltip;
 impl container::StyleSheet for Tooltip {
     fn style(&self) -> container::Style {
         container::Style {
-            text_color: Some(Color::from_rgb8(0xEE, 0xEE, 0xEE)),
-            background: Some(Background::Color([0.10, 0.21, 0.25].into())),
-            border_color: [0.7, 0.7, 0.7].into(),
+            text_color: Some(LIGHT_GREY),
+            background: Some(Background::Color(DARK_TEAL)),
+            border_color: MEDIUM_GREY,
             border_width: 1.0,
             ..container::Style::default()
         }
