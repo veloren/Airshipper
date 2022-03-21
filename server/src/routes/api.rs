95c30f12402af3da430ac86334926cae0874818b
Shouvik Ghosh <91585022+ShouvikGhosh2048@users.noreply.github.com>
2022-03-21 21:45:06 +0530

    Remove urls which don't seperate os and arch

--- server/src/routes/api.rs
+++ server/src/routes/api.rs
@@ -26,30 +26,8 @@ pub async fn channels(
     Json(result)
 }
 
-// If no channel specified we default to nightly.
-// NOTE: We want to change this behaviour once stable releases are more used than nightly
-#[get("/version/<platform>")]
-pub async fn version(db: crate::DbConnection, platform: String) -> Result<String> {
-    match db.get_latest_version(platform, "", "nightly").await? {
-        Some(ver) => Ok(ver),
-        None => Err(Status::NotFound.into()),
-    }
-}
-
-#[get("/version/<platform>/<channel>")]
-pub async fn channel_version(
-    db: crate::DbConnection,
-    platform: String,
-    channel: String,
-) -> Result<String> {
-    match db.get_latest_version(platform, "", channel).await? {
-        Some(ver) => Ok(ver),
-        None => Err(Status::NotFound.into()),
-    }
-}
-
 #[get("/version/<os>/<arch>/<channel>")]
-pub async fn channel_platform_version(
+pub async fn version(
     db: crate::DbConnection,
     os: String,
     arch: String,
@@ -61,41 +39,8 @@ pub async fn channel_platform_version(
     }
 }
 
-// If no channel specified we default to nightly.
-// NOTE: We want to change this behaviour once stable releases are more used than nightly
-#[get("/latest/<platform>")]
-pub async fn download(
-    db: crate::DbConnection,
-    metrics: &State<Arc<Metrics>>,
-    platform: String,
-) -> Result<Redirect> {
-    match db.get_latest_uri(&platform, "", "nightly").await? {
-        Some(uri) => {
-            metrics.increment(&platform, "");
-            Ok(Redirect::to(uri))
-        },
-        None => Err(Status::NotFound.into()),
-    }
-}
-
-#[get("/latest/<platform>/<channel>")]
-pub async fn channel_download(
-    db: crate::DbConnection,
-    metrics: &State<Arc<Metrics>>,
-    platform: String,
-    channel: String,
-) -> Result<Redirect> {
-    match db.get_latest_uri(&platform, "", channel).await? {
-        Some(uri) => {
-            metrics.increment(&platform, "");
-            Ok(Redirect::to(uri))
-        },
-        None => Err(Status::NotFound.into()),
-    }
-}
-
 #[get("/latest/<os>/<arch>/<channel>")]
-pub async fn channel_platform_download(
+pub async fn download(
     db: crate::DbConnection,
     metrics: &State<Arc<Metrics>>,
     os: String,
