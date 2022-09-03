f68bd7cb2c967a89d9ee28072974ea7cc9dbe018
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-09-03 22:48:41 +0300

    build(nix): refactor and update flake

--- flake.nix
+++ flake.nix
@@ -5,18 +5,63 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    nixCargoIntegration = {
+    nci = {
       url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs:
-    inputs.nixCargoIntegration.lib.makeOutputs {
+  outputs = inputs: let
+    lib = inputs.nixpkgs.lib;
+    cleanedSrc = builtins.path {
+      name = "airshipper-source";
+      path = toString ./.;
+      filter = path: type:
+        lib.all
+        (n: builtins.baseNameOf path != n)
+        [
+          "rust-toolchain"
+          "rustfmt.toml"
+          "shell.nix"
+          "default.nix"
+          "flake.nix"
+          "flake.lock"
+          "TROUBLESHOOTING.md"
+          "CONTRIBUTING.md"
+          "CHANGELOG.md"
+          "CODE_OF_CONDUCT.md"
+          "WORKFLOW.md"
+          "PACKAGING.md"
+        ];
+    };
+  in
+    inputs.nci.lib.makeOutputs {
       root = ./.;
       defaultOutputs = {
         app = "airshipper";
         package = "airshipper";
       };
-    };
+      overrides.crates = common: _: {
+        airshipper = prev: {
+          src = cleanedSrc;
+        };
+        airshipper-server = prev: {
+          src = cleanedSrc;
+        };
+      };
+      perCrateOverrides = {
+        airshipper.packageMetadata = prev: {
+          runtimeLibs = [
+            "vulkan-loader"
+            "wayland"
+            "wayland-protocols"
+            "libxkbcommon"
+            "xorg.libX11"
+            "xorg.libXrandr"
+            "xorg.libXi"
+            "xorg.libXcursor"
+          ];
+        };
+      };
+    };  
 }
