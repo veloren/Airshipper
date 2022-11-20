f77e908422fa11ca9c43f55a5caafb28037153ec
Joshua Barretto <joshua.s.barretto@gmail.com>
2022-11-20 11:39:53 +0000

    Better config CLUI

--- client/src/main.rs
+++ client/src/main.rs
@@ -25,7 +25,10 @@ pub type Result<T> = std::result::Result<T, ClientError>;
 fn main() {
     error::panic_hook();
 
-    if let Err(e) = cli::process() {
+    if let Err(e) = cli::process()
+        // If we fail to read a line, the user probably cancelled an action
+        && !matches!(e, ClientError::ReadlineError)
+    {
         tracing::error!("{}", e);
         tracing::info!("Press enter to exit...");
         let _ = std::io::stdin().read_line(&mut String::new());
