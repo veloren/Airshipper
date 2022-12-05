6f7091fde10ff0a0635841abe5d69f13f7a870b6
Ben Wallis <atomyc@gmail.com>
2022-12-05 20:38:02 +0000

    Added hover to browser buttons

--- client/src/gui/style/button.rs
+++ client/src/gui/style/button.rs
@@ -83,7 +83,7 @@ impl button::StyleSheet for AirshipperTheme {
             ButtonStyle::ServerListEntry(ServerListEntryButtonState::NotSelected) => {
                 server_list_entry_not_selected_style_active()
             },
-            ButtonStyle::Browser(style) => browser_button_style(*style),
+            ButtonStyle::Browser(style) => browser_button_style_active(*style),
             ButtonStyle::NextPrev => next_prev_button_style(),
             ButtonStyle::Transparent => transparent_button_style(),
             ButtonStyle::Settings => settings_button_style_active(),
@@ -111,6 +111,7 @@ impl button::StyleSheet for AirshipperTheme {
             ButtonStyle::ServerListEntry(ServerListEntryButtonState::NotSelected) => {
                 server_list_entry_not_selected_style_hovered()
             },
+            ButtonStyle::Browser(style) => browser_button_style_hovered(*style),
             ButtonStyle::ServerBrowser => server_browser_button_style_hovered(),
             ButtonStyle::Settings => settings_button_style_hovered(),
             _ => self.active(style), // Fallback to no hover style
@@ -137,12 +138,7 @@ fn active_download_button_style(background_color: Color) -> Appearance {
 
 fn hovered_download_button_style(background_color: Color) -> Appearance {
     Appearance {
-        background: Some(Background::Color(Color::from_rgba(
-            background_color.r * 1.1,
-            background_color.g * 1.1,
-            background_color.b * 1.1,
-            background_color.a,
-        ))),
+        background: Some(Background::Color(color_multiply(background_color, 1.1))),
         text_color: Color::WHITE,
         border_radius: 4.0,
         ..Appearance::default()
@@ -186,32 +182,37 @@ fn server_list_entry_not_selected_style_active() -> Appearance {
 
 fn server_list_entry_not_selected_style_hovered() -> Appearance {
     Appearance {
-        background: Some(Background::Color(Color::from_rgba(
-            VERY_DARK_GREY.r * 1.2,
-            VERY_DARK_GREY.g * 1.2,
-            VERY_DARK_GREY.b * 1.2,
-            VERY_DARK_GREY.a,
-        ))),
+        background: Some(Background::Color(color_multiply(VERY_DARK_GREY, 1.2))),
         text_color: Color::WHITE,
         shadow_offset: Vector::new(0.0, 0.0),
         ..Appearance::default()
     }
 }
 
-fn browser_button_style(style: BrowserButtonStyle) -> Appearance {
-    let color = match style {
+fn browser_button_style_to_color(style: BrowserButtonStyle) -> Color {
+    match style {
         BrowserButtonStyle::Discord => DISCORD_BLURPLE,
         BrowserButtonStyle::Gitlab => LIME_GREEN,
         BrowserButtonStyle::Extra => LIME_GREEN,
         BrowserButtonStyle::Youtube => YOUTUBE_RED,
         BrowserButtonStyle::Mastodon => MASTODON_PURPLE,
         BrowserButtonStyle::Reddit => REDDIT_ORANGE,
-    };
+    }
+}
 
+fn browser_button_style_active(style: BrowserButtonStyle) -> Appearance {
     Appearance {
-        background: Some(Background::Color(color)),
+        background: Some(Background::Color(browser_button_style_to_color(style))),
         border_radius: 25.0,
         ..Appearance::default()
+    }
+}
+
+fn browser_button_style_hovered(style: BrowserButtonStyle) -> Appearance {
+    let color = color_multiply(browser_button_style_to_color(style), 1.1);
+    Appearance {
+        background: Some(Background::Color(color)),
+        ..browser_button_style_active(style)
     }
 }
 
@@ -264,12 +265,16 @@ fn server_browser_button_style_active() -> Appearance {
 
 fn server_browser_button_style_hovered() -> Appearance {
     Appearance {
-        background: Some(Background::Color(Color::from_rgba(
-            CORNFLOWER_BLUE.r * 1.1,
-            CORNFLOWER_BLUE.g * 1.1,
-            CORNFLOWER_BLUE.b * 1.1,
-            CORNFLOWER_BLUE.a,
-        ))),
+        background: Some(Background::Color(color_multiply(CORNFLOWER_BLUE, 1.1))),
         ..server_browser_button_style_active()
     }
+}
+
+fn color_multiply(color: Color, multiplier: f32) -> Color {
+    return Color::new(
+        (color.r * multiplier).clamp(0.0, 1.0),
+        (color.g * multiplier).clamp(0.0, 1.0),
+        (color.b * multiplier).clamp(0.0, 1.0),
+        color.a,
+    );
 }
